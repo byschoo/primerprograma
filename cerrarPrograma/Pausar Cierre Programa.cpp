704d0c2feda43681b19e58ff7f8a53447e66847a
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2023-03-15 20:57:36 +0100

    Funcionalidades de cierre pausado en Windows. Snippets cpp creado VSCode

---
+++ cerrarPrograma/Pausar Cierre Programa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main ()
+{
+    cout << "\n Háblame Byschoo \n" << endl;
+
+    cout << "Press ENTER to exit the program" << endl;
+    system("PAUSE");
+    return 0;
+}
